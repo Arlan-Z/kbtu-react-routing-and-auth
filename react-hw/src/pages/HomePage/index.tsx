ffcbb3c436baebe6bfdae4fec9e465b7224a812c
Arlan <145493122+Arlan-Z@users.noreply.github.com>
2025-11-16 20:49:06 +0500

    Added HomePage

--- react-hw/src/pages/HomePage/index.tsx
+++ react-hw/src/pages/HomePage/index.tsx
@@ -1,7 +1,15 @@
+import "./HomePage.css";
+
 export default function HomePage() {
   return (
     <div className="home-page-wrapper">
-      <h1>Hello World</h1>
+      <div className="home-content">
+        <h1 className="home-title">Welcome to Rick and Morty Wiki</h1>
+        <p className="home-text">
+          Discover characters, episodes and locations from the multiverse of
+          Rick and Morty. Use the navigation above to explore the data from the official API.
+        </p>
+      </div>
     </div>
   );
 }
